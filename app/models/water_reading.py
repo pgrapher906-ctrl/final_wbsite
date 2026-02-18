e4fe6ec3a4e4043a1831a4c991999cbf9f18c367
pgrapher906-ctrl <pgrapher906@gmail.com>
2026-02-18 15:29:10 +0530

    Update water_reading.py

--- app/models/water_reading.py
+++ app/models/water_reading.py
@@ -1,22 +1,23 @@
-from app import db
-from datetime import datetime
+from . import db
 
 class WaterReading(db.Model):
     __tablename__ = 'water_readings'
+    
     id = db.Column(db.Integer, primary_key=True)
-    project_type = db.Column(db.String(20), nullable=False) #
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
-    pin_id = db.Column(db.String(50))
-    image_url = db.Column(db.String(255))
+    project_type = db.Column(db.String(20), nullable=False)
+    
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    lat = db.Column(db.Float, nullable=False)
+    lon = db.Column(db.Float, nullable=False)
+    pin_id = db.Column(db.String(50), nullable=False)
+    
     ph = db.Column(db.Float)
     tds = db.Column(db.Float)
-    temperature = db.Column(db.Float)
-    chlorophyll = db.Column(db.Float)
-    ta = db.Column(db.Float) # Ocean Specific
-    dic = db.Column(db.Float) # Ocean Specific
-    do = db.Column(db.Float)  # Pond Specific
+    temp = db.Column(db.Float)
+    do = db.Column(db.Float, nullable=True)
+    
+    image_url = db.Column(db.String(500))
 
-    def to_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+    def __repr__(self):
+        return f'<{self.project_type} Reading {self.pin_id}>'

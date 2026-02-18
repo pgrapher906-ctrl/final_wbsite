585e39846819db54e680140d88151b2409e03ed3
pgrapher906-ctrl <pgrapher906@gmail.com>
2026-02-19 02:25:41 +0530

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,17 +1,14 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    
-    # Navbar Tracking Fields
+    password_hash = db.Column(db.String(256), nullable=False)
     visit_count = db.Column(db.Integer, default=0)
-    last_login = db.Column(db.DateTime)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)

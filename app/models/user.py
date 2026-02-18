061c1b2a35e7a36f95c75c036ce6637384b96ccd
pgrapher906-ctrl <pgrapher906@gmail.com>
2026-02-18 12:19:28 +0530

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -16,25 +16,4 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-class WaterReading(db.Model):
-    __tablename__ = 'water_readings'
-    id = db.Column(db.Integer, primary_key=True)
-    project_type = db.Column(db.String(20), nullable=False) # 'Ocean' or 'Pond'
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
-    pin_id = db.Column(db.String(50))
-    image_url = db.Column(db.String(255))
-    
-    # Parameters
-    ph = db.Column(db.Float)
-    tds = db.Column(db.Float)
-    temperature = db.Column(db.Float)
-    chlorophyll = db.Column(db.Float)
-    ta = db.Column(db.Float) # Ocean Specific
-    dic = db.Column(db.Float) # Ocean Specific
-    do = db.Column(db.Float) # Pond Specific
-
-    def to_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        

94738da8eb06e592b4c203424edd1b25a10ac25f
pgrapher906-ctrl <pgrapher906@gmail.com>
2026-02-18 14:16:55 +0530

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -6,13 +6,14 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False) #
     password_hash = db.Column(db.String(128), nullable=False)
-    visit_count = db.Column(db.Integer, default=0)
-    last_login = db.Column(db.DateTime)
+    visit_count = db.Column(db.Integer, default=0) #
+    last_login = db.Column(db.DateTime) #
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+        

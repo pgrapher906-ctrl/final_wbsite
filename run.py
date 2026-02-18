f17bf69ad112c00993d6ab0a34c8dc030006f191
pgrapher906-ctrl <pgrapher906@gmail.com>
2026-02-18 16:39:38 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,7 @@
 import os
 from app import create_app
-from app.models import db  # Correctly import db from the models folder
 
-# Initialize the app (Removed the argument causing the error)
 app = create_app()
-
-# Automated Table Creation for Render
-with app.app_context():
-    db.create_all()
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))

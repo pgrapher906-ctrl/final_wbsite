db6fd991bb2053e7ee5ae30688268e50bc63ad8c
pgrapher906-ctrl <pgrapher906@gmail.com>
2026-02-18 13:52:13 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,11 @@ def create_app(config_class=Config):
 
     db.init_app(app)
 
+    with app.app_context():
+        from app.models.user import User
+        from app.models.water_reading import WaterReading
+        db.create_all() 
+
     from app.routes import main_bp
     app.register_blueprint(main_bp)
 

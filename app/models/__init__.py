1bd51831117c77a8b29ba29f9e8c5a482f5768db
pgrapher906-ctrl <pgrapher906@gmail.com>
2026-02-18 16:03:31 +0530

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,32 +1,4 @@
-from flask import Flask
-from flask_login import LoginManager
-from .models import db
+# FILE: app/models/__init__.py
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
-
-    # Initialize Plugins
-    db.init_app(app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'main.login'
-    login_manager.init_app(app)
-
-    # Move Model Imports HERE (Inside the function)
-    # This breaks the circular loop
-    from .models.user import User
-    from .models.water_reading import WaterReading
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
-
-    from .routes import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-    
-    # Create tables
-    with app.app_context():
-        db.create_all()
-
-    return app
+db = SQLAlchemy()
